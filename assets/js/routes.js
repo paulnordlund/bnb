847ff1d556e88a94b9e659d7571bc831798c3bd0
Paul Nordlund <paulnordlund@gmail.com>
2016-02-02 16:25:54 -0600

    initial commit
    A profile site for a bed & breakfast.  Built using Zurb’s Foundation
    for Apps, featuring media queries, AngularJS routing, panels,
    animations, and modals

---
+++ assets/js/routes.js
@@ -0,0 +1,1 @@
+var foundationRoutes = [{"name":"reservation","url":"/reservation","animationIn":"slideInRight","path":"templates/reservation.html"},{"name":"home","url":"/","path":"templates/home.html"}]; 
